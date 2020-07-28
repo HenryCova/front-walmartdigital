a8965e1247b4be424e15dc7e7bbcddf4c348d0a5
Henry Cova <herycov@gmail.com>
2020-07-27 22:44:40 -0400

    Cambios de mejora

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname+'/dist'));
+app.use(express.static(__dirname+'/dist/walmart-app'));
 app.listen(process.env.PORT || 8080);
 app.get('*', (req, res) => {
-    res.sendFile(`./dist/index.html`); // load the single view file (angular will handle the page changes on the front-end)
+    res.sendFile(__dirname +`./dist/walmart-app/index.html`); // load the single view file (angular will handle the page changes on the front-end)
 });
 
 // app.get('/*',function(req,res){
