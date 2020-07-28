409f3d45d6ccc3db4ea66c7357de53604147d79d
Henry Cova <herycov@gmail.com>
2020-07-27 22:28:41 -0400

    Actualizando el server.js

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname+'/'));
+app.use(express.static(__dirname+'/dist'));
 app.listen(process.env.PORT || 8080);
 app.get('*', (req, res) => {
-    res.sendFile(`./front-end/dist/index.html`); // load the single view file (angular will handle the page changes on the front-end)
+    res.sendFile(`./dist/index.html`); // load the single view file (angular will handle the page changes on the front-end)
 });
+
 // app.get('/*',function(req,res){
 //     res.sendFile(path.join(__dirname+'/index.html'));
 // });
